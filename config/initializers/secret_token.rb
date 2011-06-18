6db09360461c035f948188586c5e04a5c1021ae3
Chris Boertien <chris.boertien@gmail.com>
2011-06-18 07:27:11 -0700

    Initial commit, rails 3.0.9 with Refinery 1.0.0
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zmdev::Application.config.secret_token = 'b5ab1c545fef45296e9df05f7e640f089e558dffb9682fa86a492a30d9be15e6fd265f8ba32749c3ad466251239ab020d36a2f3d9ab164d6abf3d1e4db8cb5b2'
